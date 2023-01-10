d02e3ce16a84b5f72d9f768228757d20e4d9abef
Alclenice Murphy <95700143+Alclenice@users.noreply.github.com>
2023-01-10 13:39:49 +0000

    Update multiply.js

---
+++ scripts/multiply.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz</title>
+    <link rel="stylesheet" href="style.css">
+    <title>multiply</title>
+</head>
+
+<body>
+    <audio>
+        <source src="root/audio/wrong.mp3">
+        </audio>
+    <div class="container">
+        <h1>MATH 4 KIDS</h1>
+        <ul>
+            <li><a href="index.html">Add</a></li>
+            <li><a href="subtract.html">Subtract</a></li>
+            <li class="current"><a href="multiply.html">Multiply</a></li>
+            <li><a href="divide.html">Divide</a></li>
+        </ul>
+        </nav>
+    </div>
+    </div>
+    </header>
+
+    <div class="wrapper">
+        <div class="equation">
+            <h1 id="num1" style="color:#FE4A49;"></h1>
+            <h1 style="color: #2AB7CA;">*</h1>
+            <h1 id="num2" style="color: #fed766;">1</h1>
+            <h1 style="color: #F86624;">=</h1>
+            <h1 style="color: gray;">?</h1>
+        </div>
+        <div class="answer-options">
+            <h1 id="option1">1</h1>
+            <h1 id="option2">2</h1>
+            <h1 id="option3">3</h1>
+        </div>
+    </div>
+
+    <script src="multiply.js"></script>
+
+</body>
+
+</html>
+
